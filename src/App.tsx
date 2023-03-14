e3ce9a95b68812fe9785542367e524b03e71472a
svitjojo <kornotavr@gmail.com>
2023-03-14 20:16:50 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.scss';
 import React from 'react';
+import './App.scss';
+import logo from './logo.svg';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./logo.svg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
