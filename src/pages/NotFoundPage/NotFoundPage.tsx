4130aec676125a766231618c6fd7e1cada4c952f
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-16 21:40:15 +0200

    NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import './NotFoundPage.scss';
 
 export const PageNotFound: React.FC = () => (
   <div className="page-not-found__block">
-    <h1 className="page-not-found__block__title">404 page not found</h1>
+    <h1 className="page-not-found__block__title">Page not found</h1>
     <img
       className="page-not-found__block__image"
       src="https://i.pinimg.com/originals/fc/c7/f1/fcc7f1a702b158a9125831f1c0a400b9.jpg"
