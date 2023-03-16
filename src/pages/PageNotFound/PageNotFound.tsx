097b6a6dd7b7f57875f122ed47f46022884b5ba2
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-16 09:53:19 +0200

    page not found implemented

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+import './PageNotFound.scss';
+
+export const PageNotFound: React.FC = () => (
+  <div className="page-not-found__block">
+    <h1 className="page-not-found__block__title">404 page not found</h1>
+    <img
+      className="page-not-found__block__image"
+      src="https://i.pinimg.com/originals/fc/c7/f1/fcc7f1a702b158a9125831f1c0a400b9.jpg"
+    />
+  </div>
+);
