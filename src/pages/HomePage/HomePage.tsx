fb5b3d3cf3c62e9bc9dddbe583732c71bdabee6c
svitjojo <kornotavr@gmail.com>
2023-03-19 00:57:30 +0200

    CategoryCard and categories section implemented

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,58 @@
-export const HomePage = () => (
-  <div className="home-page container">
-    <div className="row">
-      <div className="col-24">
-        <h1 className="heading-1">Home Page</h1>
+import { CategoryCard } from '../../components/CategoryCard';
+
+export const HomePage = () => {
+  const categories = [
+    {
+      id: 1,
+      name: 'Mobile phones',
+      itemsCount: 94,
+      img: 'https://i.ibb.co/9tRcHMV/category-phones.png',
+      path: 'phones',
+    },
+    {
+      id: 2,
+      name: 'Tablets',
+      itemsCount: 54,
+      img: 'https://i.ibb.co/ZfxmNQ4/category-tablets.png',
+      path: 'tablets',
+    },
+    {
+      id: 3,
+      name: 'Accessories',
+      itemsCount: 104,
+      img: 'https://i.ibb.co/cCqB3t2/category-accessories.png',
+      path: 'accessories',
+    },
+  ];
+
+  return (
+    <div className="home-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-24">
+            <h1 className="heading-1">Home Page</h1>
+          </div>
+        </div>
       </div>
+
+      <section className="home-page_categories">
+        <div className="container">
+          <div className="row">
+            <h2 className="col-24 heading-2">Shop be category</h2>
+          </div>
+          <div className="row">
+            {categories.map((category) => (
+              <CategoryCard
+                name={category.name}
+                itemsCount={category.itemsCount}
+                img={category.img}
+                path={category.path}
+                key={category.id}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
-);
+  );
+};
