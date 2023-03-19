c71a840aea91c51fb4c2166937e28071349896db
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-19 23:49:36 +0200

    banner swiper, contacts, rights, footer links

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
+import { BannerSwiper } from '../../components/BannerSwiper';
+import './HomePage.scss';
+
 export const HomePage = () => (
   <div className="home-page container">
     <div className="row">
       <div className="col-24">
-        <h1 className="heading-1">Home Page</h1>
+        <h1 className="heading-1 margin-top-50">
+          Welcome to Nice Gadgets store!
+        </h1>
       </div>
     </div>
+    <div className="row margin-bot-30"></div>
+    <BannerSwiper />
   </div>
 );
