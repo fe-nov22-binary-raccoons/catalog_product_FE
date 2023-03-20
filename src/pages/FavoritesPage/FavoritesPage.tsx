f3b2b43c3854b5e2dd3d87665e41f8aa1b0f5ee2
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-03-20 19:21:16 +0200

    added favorites page layout & fixes

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,9 +1,63 @@
-export const FavoritesPage: React.FC = () => (
-  <div className="home-page container">
-    <div className="row">
-      <div className="col-24">
-        <h1 className="heading-1">Favorites Page</h1>
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getProducts } from '../../api/fetchProducts';
+import { BreadCrumbs } from '../../components/BreadCrumbs';
+import { ProductsList } from '../../components/ProductsList';
+import { Phone } from '../../types/Phone';
+
+export const FavoritesPage: React.FC = () => {
+  const [products, setProducts] = useState<Phone[]>([]);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [productsNum, setProductsNum] = useState<number>(0);
+
+  const [searchParams] = useSearchParams();
+  const currentPage = searchParams.get('page') || '1';
+  // const pageSize = searchParams.get('size') || '16';
+  const pageSize = '5';
+
+  const loadProducts = async () => {
+    setIsLoading(true);
+
+    try {
+      setIsError(false);
+      const productsFromServer = await getProducts(
+        'phones',
+        +currentPage,
+        +pageSize,
+      );
+
+      setProducts(productsFromServer.products);
+      setProductsNum(productsFromServer.total);
+    } catch (error) {
+      setIsError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, [pageSize, currentPage]);
+
+  return (
+    <div className="products">
+      <div className="container">
+        <BreadCrumbs />
+
+        <div className="row">
+          <div className="col-24">
+            <h1 className="heading-1">Favorites Page</h1>
+            <p className="subtitle">{productsNum} models</p>
+          </div>
+        </div>
       </div>
+
+      <ProductsList
+        products={products}
+        isError={isError}
+        isLoading={isLoading}
+      />
     </div>
-  </div>
-);
+  );
+};
