5631eb9981aab9d946fda01e20144345dd09da12
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-03-21 00:14:26 +0200

    added loader & button to FavPage & improved CustromSelect

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './Loader.scss';
 
 export const Loader: React.FC = () => (
-  <div className="Loader" data-cy="loader">
-    <div className="Loader__content" />
+  <div className="loader-wrap" data-cy="loader">
+    <div className="loader">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
   </div>
 );
