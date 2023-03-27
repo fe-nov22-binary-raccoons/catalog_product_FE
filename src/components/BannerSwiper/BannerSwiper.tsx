4c79e153ce6717ecb77ab7ab52cbbc0d72b276a9
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-27 09:47:59 +0300

    deleted banner with mentors, favicon changed

--- src/components/BannerSwiper/BannerSwiper.tsx
+++ src/components/BannerSwiper/BannerSwiper.tsx
@@ -21,7 +21,6 @@ import Banner2 from '../../images/bannerSwiper/banner2-wide.png';
 import Banner2Small from '../../images/bannerSwiper/banner2-320.png';
 import Banner3 from '../../images/bannerSwiper/banner3-wide.png';
 import Banner3Small from '../../images/bannerSwiper/banner3-320.png';
-import Banner4 from '../../images/bannerSwiper/banner4-wide.png';
 
 export const BannerSwiper: React.FC = () => {
   const prevRef = useRef<HTMLDivElement>(null);
@@ -60,12 +59,6 @@ export const BannerSwiper: React.FC = () => {
     },
     {
       id:3,
-      wide: Banner4,
-      small: Banner320,
-      link: 'https://youtu.be/hp_-RlwNg04?t=65',
-    },
-    {
-      id:4,
       wide: Banner3,
       small: Banner3Small,
       link: '#/phones/apple-iphone-11-128gb-purple',
